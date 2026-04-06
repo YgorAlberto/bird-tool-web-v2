7f0d8a49ea1c553ef7d845efc94dfa6666141c26
Ygor Alberto <30332359+YgorAlberto@users.noreply.github.com>
2026-04-06 16:33:14 -0300

    Update parsing-domains.sh

--- parsing-domains.sh
+++ parsing-domains.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd OUT-WEB-BIRD/
 target=$(cat ../target.txt)
-grep -riah ".$target" | grep -v "grep" | grep -v "Trying" | grep -v "Scraping" | grep -v "\-\-\-" |grep -v "IN" | cut -d " " -f 1 | grep -via "\.arpa" | grep ".$target" | grep -via http | sort -u > ../target-full.txt
+grep -riah ".$target" | grep -v "grep" | grep -v "Trying" | grep -v "Scraping" | grep -v "\-\-\-" |grep -v "IN" | cut -d " " -f 1 | grep -via "\.arpa" | grep ".$target" | grep -via http | cut -d "," -f 1| sort -u > ../target-full.txt
 
